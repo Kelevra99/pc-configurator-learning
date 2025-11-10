0cf65793a9598bf463361de20f8b30c4bae948ec
Юрий <yuriy@MacBook-Air-Urij.local>
2025-11-10 09:39:38 +0300

    Уточнение поведения описаний программы

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const contents = document.querySelectorAll('.program-line__content');
+console.log(contents)
 
 contents.forEach((elem) => {
     
     const title = elem.querySelector('.program-line__title');
-    
+   
+
+
     // title.onclick = () => {
     //    console.log(title)
     // }
@@ -12,6 +15,14 @@ const descr = elem.querySelector('.program-line__descr');
 
     title.addEventListener('click', () => {
         descr.classList.toggle('active');
+
+    // деактивируем другие описания
+    contents.forEach((otherElem) => {
+        if (otherElem !== elem) {
+            const otherDescr = otherElem.querySelector('.program-line__descr');
+            otherDescr.classList.remove('active');
+        }
+    });
     })
 
 // console.log(descr)
